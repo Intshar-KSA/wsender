9b81ff2f1484c7e003e76df7941efe1ec39cf777
sulaiman <mohammedalrabeai@gmail.com>
2024-11-03 15:42:33 +0300

    الا

--- app/Filament/Resources/CampaignResource/Pages/EditCampaign.php
+++ app/Filament/Resources/CampaignResource/Pages/EditCampaign.php
@@ -9,6 +9,10 @@ use Filament\Resources\Pages\EditRecord;
 class EditCampaign extends EditRecord
 {
     protected static string $resource = CampaignResource::class;
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 
     protected function getHeaderActions(): array
     {
--- app/Filament/Resources/ChatBotResource/Pages/EditChatBot.php
+++ app/Filament/Resources/ChatBotResource/Pages/EditChatBot.php
@@ -10,6 +10,11 @@ class EditChatBot extends EditRecord
 {
     protected static string $resource = ChatBotResource::class;
 
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
     protected function getHeaderActions(): array
     {
         return [
