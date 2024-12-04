275952105c1232e506c36c9d6d1745e34269c3b8
sulaiman <mohammedalrabeai@gmail.com>
2024-12-04 15:02:13 +0300

    ss

--- app/Filament/Resources/DeviceResource.php
+++ app/Filament/Resources/DeviceResource.php
@@ -23,6 +23,7 @@ class DeviceResource extends Resource
     protected static ?string $model = Device::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?int $navigationSort = -2;
 
     public static function form(Form $form): Form
     {
