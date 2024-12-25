108f6127287987b4030c84e3ae957de2398bcfd9
mohammed <mohammedalrabeai@gmail.com>
2024-12-25 14:05:55 +0300

    gg

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\WebhookController;
 
 Route::get('/user', function (Request $request) {
