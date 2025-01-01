d989f380b936db46fcdd7cd03bb3cc27cdba1605
mohammed <mohammedalrabeai@gmail.com>
2025-01-01 12:39:27 +0300

    dd

--- database/seeders/PlanSeeder.php
+++ database/seeders/PlanSeeder.php
@@ -9,38 +9,38 @@ class PlanSeeder extends Seeder
 {
     public function run(): void
     {
-        // if (Plan::count() == 0) {
-        //     Plan::create([
-        //         'title' => 'Free Plan',
-        //         'number_of_days' => 0,
-        //         'price' => 0,
-        //         'is_free' => true,
-        //         'hours' => 20,
-        //     ]);
+        if (Plan::count() == 0) {
+            Plan::create([
+                'title' => 'Free Plan',
+                'number_of_days' => 0,
+                'price' => 0,
+                'is_free' => true,
+                'hours' => 20,
+            ]);
 
-        //     Plan::create([
-        //         'title' => 'Monthly Plan',
-        //         'number_of_days' => 30,
-        //         'price' => 100,
-        //     ]);
+            Plan::create([
+                'title' => 'Monthly Plan',
+                'number_of_days' => 30,
+                'price' => 100,
+            ]);
 
-        //     Plan::create([
-        //         'title' => 'Three Months Plan',
-        //         'number_of_days' => 90,
-        //         'price' => 270,
-        //     ]);
+            Plan::create([
+                'title' => 'Three Months Plan',
+                'number_of_days' => 90,
+                'price' => 270,
+            ]);
 
-        //     Plan::create([
-        //         'title' => 'Six Months Plan',
-        //         'number_of_days' => 180,
-        //         'price' => 500,
-        //     ]);
+            Plan::create([
+                'title' => 'Six Months Plan',
+                'number_of_days' => 180,
+                'price' => 500,
+            ]);
 
-        //     Plan::create([
-        //         'title' => 'Yearly Plan',
-        //         'number_of_days' => 365,
-        //         'price' => 900,
-        //     ]);
-        // }
+            Plan::create([
+                'title' => 'Yearly Plan',
+                'number_of_days' => 365,
+                'price' => 900,
+            ]);
+        }
     }
 }
