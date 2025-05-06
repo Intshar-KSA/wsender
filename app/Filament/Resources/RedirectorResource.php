6ce3fba17f0f5155a95ac091f6d078855384bd0b
mohammed <33159671+mohammedALrabeai@users.noreply.github.com>
2025-05-06 18:00:03 +0300

    jjj

--- app/Filament/Resources/RedirectorResource.php
+++ app/Filament/Resources/RedirectorResource.php
@@ -8,13 +8,18 @@ class RedirectorResource extends Resource
 {
     protected static ?string $model = null; // لا يوجد موديل فعلي
 
-    protected static ?string $navigationLabel = 'وثائق API';
+    // protected static ?string $navigationLabel = 'وثائق API';
 
     protected static ?string $navigationIcon = 'heroicon-o-book-open';
 
     protected static ?string $navigationGroup = 'الدعم';
 
     protected static ?int $navigationSort = 100;
+
+    public static function getModelLabel(): string
+    {
+        return _('API Documentation');
+    }
 
     public static function getPages(): array
     {
